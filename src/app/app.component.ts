0f2bbdbc8659699de67f3bbc0c6e5649f96a5fcc
System Administrator <root@macadmins-MacBook-Pro-2.local>
2018-07-28 12:40:39 +0530

    changes in console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      console.log("Platform is ready");
+      console.log("Platform is ready Changes by Dhiraj");
       statusBar.styleDefault();
       splashScreen.hide();
     });
